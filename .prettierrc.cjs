341b6fffc1b6fcf4a30e438a5a37b7889e4130f1
oahz <pengoahz@gmail.com>
2023-12-10 19:12:12 +0800

    chore: up

---
+++ .prettierrc.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require("@0ahz/prettier-config"),
+  plugins: ["prettier-plugin-tailwindcss"],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      height: {
+        screen: ['100vh', '100dvh'],
+      },
+      minHeight: {
+        screen: ['100vh', '100dvh'],
+      },
+      maxHeight: {
+        screen: ['100vh', '100dvh'],
+      },
+    },
+  },
+  plugins: [],
+};
